00bd183315a88823dcf7eceeaae747456f3c478c
Sofia <sofia-pomaaltamirano@hotmail.com>
2022-07-17 16:09:02 -0500

    actualizacion

--- src/main/java/Solid/LSP/PaymentHelper.java
+++ src/main/java/Solid/LSP/PaymentHelper.java
@@ -1,11 +1,29 @@
 package Solid.LSP;
-/*
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PaymentHelper {
-    ...completa
+    List<NewPayment> newPayments = new ArrayList<NewPayment>();
+    List<PreviousPayment> previousPayments = new ArrayList<PreviousPayment>();
+
+    public void addNewPayment(NewPayment user){
+        newPayments.add(user);
+    }
+    public void addPreviousPayment(PreviousPayment user){
+        previousPayments.add(user);
+    }
+    public void showPreviousPayments() {
+        for (PreviousPayment payment: previousPayments) {
+            payment.previousPaymentInfo();
+            System.out.println("------");
         }
     }
+    public void processNewPayments()  {
+        for (NewPayment payment: newPayments) {
+            payment.newPayment();
+            System.out.println("------");
+        }
+    }
+
 }
-*/
